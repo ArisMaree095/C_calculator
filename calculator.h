2ef19c3813a067af662759a07776dc41b56e07e9
Ariana MVP <arianamarievp@gmail.com>
2026-08-24 13:32:22 -0400

    Fix header guard in calculator.h

--- calculator.h
+++ calculator.h
@@ -5,9 +5,10 @@
 #ifndef C_CALCULATOR_CALCULATOR_H
 #define C_CALCULATOR_CALCULATOR_H
 
-#endif //C_CALCULATOR_CALCULATOR_H
 
 double add(double num1, double num2);
 double subtract(double num1, double num2);
 double multiply(double num1, double num2);
 double divide(double num1, double num2);
+
+#endif //C_CALCULATOR_CALCULATOR_H
